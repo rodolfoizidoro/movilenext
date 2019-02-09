28eac09a3662b25b7ba0996b78c9d580579147ca
Rodolfo Izidoro <rodolfo.morais@getninjas.com.br>
2019-02-09 11:59:20 -0200

    implement new word activity

--- app/src/main/java/com/example/rodolfoizidoro/movilenext/view/Main2Activity.kt
+++ app/src/main/java/com/example/rodolfoizidoro/movilenext/view/Main2Activity.kt
@@ -1,5 +1,7 @@
 package com.example.rodolfoizidoro.movilenext.view
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +10,17 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rodolfoizidoro.movilenext.R
 import com.example.rodolfoizidoro.movilenext.adapter.WordListAdapter
+import com.example.rodolfoizidoro.movilenext.entities.Word
 import com.example.rodolfoizidoro.movilenext.viewmodel.WordViewModel
 
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.content_main2.*
+import org.jetbrains.anko.toast
 
 class Main2Activity : AppCompatActivity() {
+    companion object {
+        const val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
+    }
 
     private lateinit var wordViewModel: WordViewModel
 
@@ -31,9 +38,30 @@ class Main2Activity : AppCompatActivity() {
             adapter.words = it
         })
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(
+                this@Main2Activity,
+                NewWordActivity::class.java
+            )
+            startActivityForResult(
+                intent,
+                NEW_WORD_ACTIVITY_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE &&
+            resultCode == Activity.RESULT_OK
+        ) {
+            data?.let {
+                val word =
+                    Word(it.getStringExtra(NewWordActivity.EXTRA_REPLY))
+                wordViewModel.insert(word)
+            }
+        } else {
+            toast(R.string.empty_not_saved)
         }
     }
 }
